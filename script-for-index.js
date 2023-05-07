13565c01d77ae9738f6d98fcfa6a44c80eb5a997
Lushev <kamil.cukla@wp.pl>
2023-05-07 19:21:43 +0200

    fixing issue with theme change on main page

---
+++ script-for-index.js
@@ -0,0 +1,23 @@
+const linkCss = document.getElementById('theme-link');  
+const themeSwitch = document.querySelector('.theme-switch');
+
+//changing themes
+window.onload = function() {
+  if (localStorage.getItem("mode") === "dark") {
+    linkCss.href="styles/dark.css";
+    themeSwitch.checked = true;
+  } else {
+    linkCss.href="styles/light.css";
+    
+  }
+}
+
+themeSwitch.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    linkCss.href="styles/dark.css";
+    localStorage.setItem("mode", "dark");
+  } else {
+    linkCss.href="styles/light.css";
+    localStorage.setItem("mode", "light");
+  }
+})
